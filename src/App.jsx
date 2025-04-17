c02e15aa2ae77def43584d09808e1557dbb6f4dd
jonastino21 <heryniavo@yahoo.com>
2025-04-17 06:58:02 +0000

    modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <ul
+          className="nav nav-pills nav-fill gap-2 p-1 small bg-primary rounded-5 shadow-sm"
+          id="pillNav2"
+          role="tablist"
+          style={{
+            "--bs-nav-link-color": "var(--bs-white)",
+            "--bs-nav-pills-link-active-color": "var(--bs-primary)",
+            "--bs-nav-pills-link-active-bg": "var(--bs-white)",
+          }}
+        >
+          <li className="nav-item" role="presentation">
+            <button
+              className="nav-link active rounded-5"
+              id="home-tab2"
+              data-bs-toggle="tab"
+              type="button"
+              role="tab"
+              aria-selected="true"
+            >
+              Home
+            </button>
+          </li>
+          <li className="nav-item" role="presentation">
+            <button
+              className="nav-link rounded-5"
+              id="profile-tab2"
+              data-bs-toggle="tab"
+              type="button"
+              role="tab"
+              aria-selected="false"
+            >
+              Profile
+            </button>
+          </li>
+          <li className="nav-item" role="presentation">
+            <button
+              className="nav-link rounded-5"
+              id="contact-tab2"
+              data-bs-toggle="tab"
+              type="button"
+              role="tab"
+              aria-selected="false"
+            >
+              Contact
+            </button>
+          </li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
