de9f65152d56a78a6348d21814da2f1a0ddd40f0
kananavy <kananavymiremby@gmail.com>
2025-04-22 12:34:08 +0200

    Votre message de commit

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,12 @@
+<<<<<<< HEAD
+// postcss.config.js
+export default {
+=======
+// postcss.config.cjs
 module.exports = {
-    plugins: {
-      '@tailwindcss/postcss': {},
-      autoprefixer: {},
-    },
-  };
-  
+>>>>>>> 3a27c23 (Votre message de commit)
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+}
