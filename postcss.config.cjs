f57a708eb985787537a3f938500df8c2baf4c6b5
Francky <kyfr.rakotonirina1@gmail.com>
2025-05-05 10:05:56 +0200

    fix conflit

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-// postcss.config.js
-export default {
-=======
 // postcss.config.cjs
 module.exports = {
->>>>>>> 3a27c23 (Votre message de commit)
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
