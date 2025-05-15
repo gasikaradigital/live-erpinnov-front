304073caaa5f66d6defd70f43dd40b4aaaa64934
Francky <kyfr.rakotonirina1@gmail.com>
2025-05-15 12:10:29 +0200

    modif http

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -36,7 +36,7 @@ const Login = () => {
       //   'Accept': 'application/json'
       //   },
       // });
-      const response = await axios.post("https://backend.erpinnov.com/api/login", {
+      const response = await axios.post("http://backend.erpinnov.com/api/login", {
         email,
         password,
       }, {
