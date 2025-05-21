33aa21c00b60c45ecf122fc0f3ed179dfd90ccfd
Francky <kyfr.rakotonirina1@gmail.com>
2025-05-21 09:12:44 +0200

    fix requireAuth

--- src/pages/auth/requireAuth.jsx
+++ src/pages/auth/requireAuth.jsx
@@ -15,35 +15,19 @@ const RequireAuth = () => {
     const checkAuth = async () => {
       try {
         await csrf();
-        await user();
-        setAuthenticated(true);
+        const response = await user();
+        if(response.status !== 200){
+          navigate('/', {replace: true});
+        }
       } catch (error) {
-        console.error("Erreur d'authentification :", error);
-        setAuthenticated(false);
-      } finally {
-        setLoading(false);
-      }
+        navigate('/', { replace: true });
+      } 
     };
 
     checkAuth();
-  }, []);
+  }, [navigate]);
 
-  useEffect(() => {
-    // redirection une fois la vérification terminée
-    if (authenticated === false && !loading) {
-      navigate('/', { replace: true });
-    }
-  }, [authenticated, loading, navigate]);
-
-  if (loading) {
-    return <div>Vérification en cours...</div>;
-  }
-
-  if (authenticated) {
-    return <Outlet />;
-  }
-
-  return null; // on ne retourne rien tant que la redirection n'est pas faite
+  return <div>Authentification en cours...</div>;
 };
 
 export default RequireAuth;
