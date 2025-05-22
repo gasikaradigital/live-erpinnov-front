bc135f3a26a71d9f671815b0f8e90e4ebce5d89c
haritsimba <haritsimbafranck@gmail.com>
2025-05-22 11:22:01 +0300

    auth context

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,78 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+import { api } from '../config';
+import { useNavigate } from 'react-router-dom';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [authenticated, setAuthenticated] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const checkAuth = async () => {
+            try {
+                const { data } = await api.get('/api/user');
+                setAuthenticated(!!data);
+            } catch (err) {
+                console.log("error : "+err);
+                setAuthenticated(false);
+            } finally {
+                setLoading(false);
+            }
+        };
+        checkAuth();
+    }, []);
+
+    const login = async (credentials) => {
+
+        const csrf_response = await api.get(`/sanctum/csrf-cookie`);
+
+        console.log(csrf_response.data);
+
+        setLoading(true);
+        setError(null);
+
+        try {
+            const { data } = await api.post('/api/login', credentials);
+
+            if (data.message === "Authentificated") {
+                setAuthenticated(true);
+                navigate('/dashboard');
+                return true;
+            }
+        } catch (err) {
+            setError(err.response?.data?.message || "Échec de l'authentification");
+            return false;
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const logout = async () => {
+        try {
+            await api.post('/api/logout');
+        } catch (err) {
+            setError(err.response?.data?.message || "Échec de la déconnexion");
+        } finally {
+            setAuthenticated(false);
+            navigate('/');
+        }
+    };
+
+    return (
+        <AuthContext.Provider value={{
+            authenticated,
+            loading,
+            error,
+            setAuthenticated,
+            login,
+            logout
+        }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => useContext(AuthContext);
