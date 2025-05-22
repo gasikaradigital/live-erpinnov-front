8081104d8acf0106e7e18d82961529120b9b4d03
haritsimba <haritsimbafranck@gmail.com>
2025-05-22 11:36:19 +0300

    loader

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -3,6 +3,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import { toast } from 'react-toastify';
 import { ScaleLoader } from 'react-spinners';
+import { Container } from 'react-bootstrap';
 
 const ProtectedRoute = () => {
   const { authenticated, loading } = useAuth();
@@ -13,8 +14,20 @@ const ProtectedRoute = () => {
     }
   }, [authenticated, loading]);
 
-  if (loading) return <ScaleLoader color="#1d32f3" />;
-  return authenticated ? <Outlet /> : <Navigate to="/" replace />;
+  return (
+    <>
+      {loading && (
+        <Container
+          fluid
+          className="bg-transparent py-2"
+          style={{ maxWidth: '200px' }}
+        >
+          <ScaleLoader color="#1d32f3" height={25} width={4} radius={2} />
+        </Container>
+      )}
+      {!loading && (authenticated ? <Outlet /> : <Navigate to="/" replace />)}
+    </>
+  );
 };
 
 export default ProtectedRoute;
