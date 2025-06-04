3b3c2ca2a46cffd6013ab1eb46767a432a4e2165
Francky <kyfr.rakotonirina1@gmail.com>
2025-06-04 09:10:58 +0200

    champ nom entreprise caché

--- src/components/common/InstanceCreate.jsx
+++ src/components/common/InstanceCreate.jsx
@@ -136,32 +136,35 @@ const InstanceCreate = () => {
             </div>
 
             {/* Form Section */}
-            <div className="form-section">
-              <div className="form-group">
-                <label className="form-label">
-                  Nom de l'instance <span className="required">*</span>
-                </label>
-                <div className="input-container">
-                  <div className="input-group">
-                    <span className="input-group-text">
-                      <svg width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                        <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z"/>
-                        <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z"/>
-                      </svg>
-                    </span>
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="votreinstance"
-                      value={instanceName}
-                      onChange={(e) => setInstanceName(e.target.value)}
-                    />
-                    <span className="input-group-text suffix">.erpinnov.com</span>
+            {selectedOption !== 'automatic' &&(
+              <div className="form-section">
+                <div className="form-group">
+                  <label className="form-label">
+                    Nom de l'instance <span className="required">*</span>
+                  </label>
+                  <div className="input-container">
+                    <div className="input-group">
+                      <span className="input-group-text">
+                        <svg width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                          <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z"/>
+                          <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z"/>
+                        </svg>
+                      </span>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="votreinstance"
+                        value={instanceName}
+                        onChange={(e) => setInstanceName(e.target.value)}
+                      />
+                      <span className="input-group-text suffix">.erpinnov.com</span>
+                    </div>
                   </div>
+                  <small className="helper-text">Votre nom d'instance personnalisé</small>
                 </div>
-                <small className="helper-text">Votre nom d'instance personnalisé</small>
               </div>
-            </div>
+            )}
+            
 
             {/* Enterprise Section */}
             <div className="enterprise-section">
