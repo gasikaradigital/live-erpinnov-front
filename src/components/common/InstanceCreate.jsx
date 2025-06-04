2ddf58c5b807ad2875a41a41821008f7d271b8c7
Francky <kyfr.rakotonirina1@gmail.com>
2025-06-04 09:29:02 +0200

    fix bouton créer l'instance

--- src/components/common/InstanceCreate.jsx
+++ src/components/common/InstanceCreate.jsx
@@ -224,8 +224,7 @@ const InstanceCreate = () => {
                 Réinitialiser
               </Button>
               
-              {selectedOption !== 'automatic' || instanceName.trim() !== '' ? (
-                <Button 
+              <Button 
                 variant="primary"
                 className="btn btn-primary create-btn"
                 onClick={handleCreateInstance}
@@ -236,8 +235,6 @@ const InstanceCreate = () => {
                   <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
                 </svg>
               </Button>
-              ) : null}
-              
             </div>
           </div>
 
