e6b4daf65423bda7001f2684cdc900556e06fbe3
Francky <kyfr.rakotonirina1@gmail.com>
2025-07-22 14:57:52 +0200

    test d'attente

--- src/components/common/InstanceCreate.jsx
+++ src/components/common/InstanceCreate.jsx
@@ -8,6 +8,7 @@ import { fetchEntreprises } from "../../api/enterpriseApi";
 import { createInstance } from "../../api/instanceApi";
 import { toast } from 'react-toastify';
 import { createSubscription } from '../../api/subscriptionApi';
+import { ScaleLoader } from "react-spinners";
 
 
 const InstanceCreate = () => {
@@ -18,6 +19,7 @@ const InstanceCreate = () => {
   const [instanceName, setInstanceName] = useState('');
   const [selectedOption, setSelectedOption] = useState('manual');
   const [selectedEnterprise, setSelectedEnterprise] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   const handleBackClick = () => {
     navigate(-1);
@@ -28,6 +30,7 @@ const InstanceCreate = () => {
   };
 
   const handleCreateInstance = async () => {
+    setLoading(true);
     let nameToUse;
 
     if(selectedOption === 'automatic') {
@@ -299,11 +302,16 @@ const InstanceCreate = () => {
                 onClick={handleCreateInstance}
                 disabled={selectedOption !== 'automatic' && !instanceName.trim()}
               >
-                Créer l'instance
+                {loading ? "Chargement..." : "Créer l'instance"}
                 <svg width="16" height="16" className="ms-2" fill="currentColor" viewBox="0 0 16 16">
                   <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
                 </svg>
               </Button>
+              {loading && (
+                <div style="{{ marginTop: 10 }}">
+                  <ScaleLoader color="#0d6efd" height={20} width={4} radius={2} />
+                </div>
+              )}
             </div>
           </div>
 
